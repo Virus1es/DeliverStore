19a119bf915661199a959b1bbfef9d25f6a6dee9
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-04-08 18:25:32 +0300

    refactor: second version of fabric method

--- DeliverStore.Domain/Models/Cart.cs
+++ DeliverStore.Domain/Models/Cart.cs
@@ -4,22 +4,31 @@ namespace DeliverStore.Domain.Models;
 
 public class Cart
 {
-    private Cart() { }
+    private List<Guid> _orderItemIds => [];
+
+    private Cart(Guid id, Guid customerId) { 
+        Id = id; 
+        CustomerId = customerId;
+    }
 
     public Guid Id { get; private set; }
 
     // Идентификатор на покупателя
-    public int CustomerId { get; private set; }
+    public Guid CustomerId { get; private set; }
 
     // Список идентификаторов на OrderItem
-    public List<int> OrderItemIds { get; private set; }
+    public IReadOnlyList<Guid> OrderItemIds => _orderItemIds;
 
-    public static Cart CreateNewCart(Guid id, int customerId, List<int> orderItemIds)
+    public static Result Create(Guid id, Guid customerId)
     {
-        return new Cart() {
-            Id = id, 
-            CustomerId = customerId, 
-            OrderItemIds = orderItemIds 
-        };
+        if (id == Guid.Empty)
+            Result.Failure("Invalid Id");
+
+        if (customerId == Guid.Empty)
+            Result.Failure("Invalid CustomerId for Cart");
+
+        Cart cart = new(id, customerId);
+
+        return Result.Success(cart);
     }
 }

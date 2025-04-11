363857cc4e1877ddf491cfc68f7849c1b6da8b4c
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-04-11 23:19:24 +0300

    deps: use new deps in web

--- DeliverStore.Web/Program.cs
+++ DeliverStore.Web/Program.cs
@@ -3,8 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// builder.Services.AddScoped<AppDbContext>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 

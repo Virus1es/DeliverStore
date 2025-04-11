521eb6ecfd1614e327221f78a73df1e980dac3b6
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-04-11 23:57:20 +0300

    feat: add class to response extensions

---
+++ DeliverStore.Web/Extensions/ResponseExtensions.cs
@@ -0,0 +1,25 @@
+﻿using DeliverStore.Domain.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace DeliverStore.Web.Extensions;
+
+public static class ResponseExtensions
+{
+    public static ActionResult ToErrorResponse(this Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Failture => StatusCodes.Status500InternalServerError,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return new ObjectResult(error)
+        {
+            StatusCode = statusCode
+        };
+    }
+}

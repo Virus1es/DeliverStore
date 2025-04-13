10b97606ce0ecc2caf5fd36250946cc9b8c4bf00
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-04-13 18:50:07 +0300

    feat: add new class to control errors on back

--- DeliverStore.Web/Extensions/ResponseExtensions.cs
+++ DeliverStore.Web/Extensions/ResponseExtensions.cs
@@ -1,14 +1,18 @@
-﻿using DeliverStore.Domain.Shared;
+﻿using CSharpFunctionalExtensions;
+using DeliverStore.Domain.Shared;
+using DeliverStore.Web.Response;
 using Microsoft.AspNetCore.Mvc;
-using System.Runtime.CompilerServices;
 
 namespace DeliverStore.Web.Extensions;
 
 public static class ResponseExtensions
 {
-    public static ActionResult ToErrorResponse(this Error error)
+    public static ActionResult ToResponse(this UnitResult<Error> result)
     {
-        var statusCode = error.Type switch
+        if (result.IsSuccess)
+            return new OkResult();
+
+        var statusCode = result.Error.Type switch
         {
             ErrorType.Validation => StatusCodes.Status400BadRequest,
             ErrorType.NotFound => StatusCodes.Status404NotFound,
@@ -17,7 +21,31 @@ public static class ResponseExtensions
             _ => StatusCodes.Status500InternalServerError
         };
 
-        return new ObjectResult(error)
+        var envelope = Envelope.Error(result.Error);
+
+        return new ObjectResult(envelope)
+        {
+            StatusCode = statusCode
+        };
+    }
+
+    public static ActionResult<T> ToResponse<T>(this Result<T, Error> result)
+    {
+        if (result.IsSuccess)
+            return new OkObjectResult(result.Value);
+
+        var statusCode = result.Error.Type switch
+        {
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Failture => StatusCodes.Status500InternalServerError,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        var envelope = Envelope.Error(result.Error);
+
+        return new ObjectResult(envelope)
         {
             StatusCode = statusCode
         };
---
+++ DeliverStore.Web/Response/Envelope.cs
@@ -0,0 +1,26 @@
+﻿using DeliverStore.Domain.Shared;
+
+namespace DeliverStore.Web.Response;
+
+public record Envelope
+{
+    public object? Result { get; }
+
+    public string? ErrorCode { get; }
+
+    public string? ErrorMessage { get; }
+
+    public DateTime TimeGenerated { get; }
+
+    private Envelope(object? result, Error? error)
+    {
+        Result = result; 
+        ErrorCode = error?.Code; 
+        ErrorMessage = error?.Message;
+        TimeGenerated = DateTime.Now;
+    }
+
+    public static Envelope Ok(object? result = null) => new(result, null);
+
+    public static Envelope Error(Error error) => new(null, error);
+}
